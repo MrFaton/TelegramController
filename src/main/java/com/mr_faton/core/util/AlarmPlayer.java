1407efdf734aec391fdc6a6dc024f16835a68560
root <firefly90@inbox.ru>
2016-01-29 22:31:59 +0200

    update AlarmPlayer

--- src/main/java/com/mr_faton/core/util/AlarmPlayer.java
+++ src/main/java/com/mr_faton/core/util/AlarmPlayer.java
@@ -2,15 +2,15 @@ package com.mr_faton.core.util;
 
 import org.apache.log4j.Logger;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.io.File;
+import java.net.MalformedURLException;
 
 public class AlarmPlayer {
     private static final Logger logger = Logger.getLogger("" +
             "com.mr_faton.core.util.AlarmPlayer");
-    private Clip clip;
+    private AudioClip clip;
     private boolean isAlreadyPlaying = false;
 
     public void play() {
@@ -30,20 +30,19 @@ public class AlarmPlayer {
         isAlreadyPlaying = true;
 
         try {
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(alarmSoundFile);
-            clip = AudioSystem.getClip();
-            clip.open(audioInputStream);
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-        } catch (Exception ex) {
+            clip = Applet.newAudioClip(alarmSoundFile.toURI().toURL());
+            clip.loop();
+        } catch (MalformedURLException ex) {
             logger.warn("Alarm Player exception", ex);
         }
     }
 
     public void stop() {
         logger.debug("stop playing alarm");
-        if (clip != null && clip.isRunning()) {
-            isAlreadyPlaying = false;
+        isAlreadyPlaying = false;
+        if (clip != null) {
             clip.stop();
+            clip = null;
         }
     }
 }
